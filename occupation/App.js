23aa6e5d8556ab0ba72e3c057afaccb03575f4a1
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-06-15 22:36:56 +0800

    Add title card to top of stack

--- occupation/App.js
+++ occupation/App.js
@@ -1,15 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Button,
+  TextInput,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+
+const TopStack = createStackNavigator();
+
+function TitleScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Occupation!</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Occupation!</Text>
-      
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TopStack.Navigator mode="modal" headerMode="none">
+        <TopStack.Screen
+          name="Intro"
+          component={TitleScreen}
+          options={{ headerShown: false }}
+        />
+      </TopStack.Navigator>
+    </NavigationContainer>
   );
 
 }
