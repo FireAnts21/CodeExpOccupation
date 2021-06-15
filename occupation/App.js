f03859d8d8e26f0c0cb264877bf5ffb4bd2bc6eb
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-06-15 22:47:20 +0800

    Create top of stack start page

--- occupation/App.js
+++ occupation/App.js
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { FontAwesome } from '@expo/vector-icons';
 
 
 const TopStack = createStackNavigator();
@@ -19,7 +20,12 @@ function TitleScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Occupation!</Text>
-
+      <TouchableOpacity style={styles.startButton} onPress={() => navigation.navigate("")}>
+          <Text style={styles.startBtnTxt}>
+            Let's go! 
+            <FontAwesome name="play-circle" size={24} color="black" />
+          </Text>          
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -51,6 +57,15 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 40,
     fontWeight: 'bold',
-    color: 'lightblue'
+    color: 'lightblue',
+  },
+  startBtnTxt: {
+    fontSize: 24,
+  },
+  startButton: {
+    backgroundColor: 'lightgreen',
+    padding: 10,
+    borderRadius: 10,
+    margin: 10,
   }
 });
